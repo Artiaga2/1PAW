165215cf1ef5a6a980c7cfcc79899af1a046221b
Alejandro Artiaga <alesev1993@gmail.com>
2017-03-30 17:44:04 +0200

    Examen

---
+++ ElCorteAndaluz/src/com/artiaga/Lavadoras.java
@@ -0,0 +1,76 @@
+package com.artiaga;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by artiaga on 30/3/17.
+ */
+public class Lavadoras extends Electrodomesticos {
+
+    private String tipoCarga;
+    private double velocidadCentrifugado;
+    private boolean secadoras;
+
+
+    //Constructores
+
+    public Lavadoras(String marca, String modelo, double precioBase, String consumoElectrico, int altura, int anchura, int peso, String tipoCarga, double velocidadCentrifugado, boolean secadoras) {
+        super(marca, modelo, precioBase, consumoElectrico, altura, anchura, peso);
+        this.tipoCarga = tipoCarga;
+        this.velocidadCentrifugado = velocidadCentrifugado;
+        this.secadoras = secadoras;
+    }
+
+    public Lavadoras() {
+    }
+
+    //Accesores
+
+
+    public String getTipoCarga() {
+        return tipoCarga;
+    }
+
+    public void setTipoCarga(String tipoCarga) {
+        this.tipoCarga = tipoCarga;
+    }
+
+    public double getVelocidadCentrifugado() {
+        return velocidadCentrifugado;
+    }
+
+    public void setVelocidadCentrifugado(double velocidadCentrifugado) {
+        this.velocidadCentrifugado = velocidadCentrifugado;
+    }
+
+    public boolean isSecadoras() {
+        return secadoras;
+    }
+
+    public void setSecadoras(boolean secadoras) {
+        this.secadoras = secadoras;
+
+    }
+
+    //metodos
+
+    public double ingesoTotalLavadoras(double...args){
+
+        double precio = 0;
+
+        for (int i = 0; i < args.length ; i++) {
+            precio += args[i];
+        }
+
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Lavadoras{" +
+                "tipoCarga='" + tipoCarga + '\'' +
+                ", velocidadCentrifugado=" + velocidadCentrifugado +
+                ", secadoras=" + secadoras +
+                '}';
+    }
+}
